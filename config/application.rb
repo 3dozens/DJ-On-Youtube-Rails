939f3eb6b44a4f4ab71366c1fc549fae4fd9ea94
kento_hayasaka <thunders.1028@gmail.com>
2015-10-24 23:18:16 +0900

    modify: config files

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,20 @@ module DJonYoutube
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s] #i18n(多言語に対応するためのライブラリ)の設定ファイルのディレクトリ指定
+    config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.helper false # rails generateしたときに、ヘルパーを自動生成しない
+      g.assets false # 　　　　　　　　　　　　　アセット(SCSSとCoffeeScriptファイル)
+      g.test_framework :rspec # テスティングフレームワークの設定
+      g.view_specs false # viewのテストを自動生成しない
+    end
   end
 end
