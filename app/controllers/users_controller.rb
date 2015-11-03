49f48b4408e68abb3c0d1e39aba49f813d4baaef
kento_hayasaka <thunders.1028@gmail.com>
2015-11-03 22:12:15 +0900

    add: display minimum view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def play
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'play' => 'users#play'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
