2a3d6a35f7c75e369c6a309427847b2e0af36911
kento_hayasaka <thunders.1028@gmail.com>
2016-02-04 17:32:32 +0900

    modify: separate js function to lib and main directory

---
+++ app/assets/javascripts/lib/audio.js
@@ -0,0 +1,64 @@
+
+// Web Audio APIが使えるか確認しつつ、contextをつくる
+function createContext() {
+    try {
+        var SupportedAudioContext = window.AudioContext || window.webkitAudioContext;
+    } catch (e) {
+        throw new Error('Web Audio API is not supported.');
+    }
+
+    return new SupportedAudioContext();
+}
+
+/**
+ * 複数の音声を非同期でロードし、配列に詰め込みます
+ * @param urlList ロードするURLの配列
+ * @param soundList ロードした音源を詰め込む配列
+ * @param context AudioContext
+ * @returns dfd
+ */
+function loadSounds(urlList, soundList, context) {
+    var dfd = new $.Deferred;
+
+    $.each(urlList, function(i, url) {
+        var videoId = decodeURIComponent(url.match(/.*v%3D(.*)$/)[1]); // URLからvideo idを抜き出します
+        var request = new XMLHttpRequest();
+
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer';
+        request.send();
+        request.onload = function() {
+            // contextにArrayBufferを渡し、decodeさせる
+            context.decodeAudioData(request.response, function (decoded_buf) {
+                var source = context.createBufferSource();
+                source.buffer = decoded_buf;
+                source.connect(context.destination);
+
+                var bufferObj = {videoId: videoId, source: source};
+                soundList.push(bufferObj);
+
+                if (url == $(urlList).last()[0]) dfd.resolve();
+            });
+        };
+    });
+
+    return dfd.promise();
+}
+
+function play(playSource, soundList, context) {
+    playSource.start();
+
+    var newSource = context.createBufferSource();
+    newSource.buffer = playSource.buffer;
+
+    $.each(soundList, function(i, sound) {
+       if (playSource === sound.source) {
+           soundList[i].source = newSource;
+           soundList[i].videoId = "test"; // debug
+           //sound.source = newsSource;
+           return false; // break
+       }
+    });
+
+    //return newSource;
+}
---
+++ app/assets/javascripts/main/play.coffee
@@ -0,0 +1,34 @@
+$ ->
+
+  # 音声ファイルの場所
+  SOUND_URLS = ['http://192.168.33.10:3000/music-request?video_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Df0kXY7MQHLw%26',
+               'http://192.168.33.10:3000/music-request?video_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D0H24mH17oJw'
+  ]
+
+  context = createContext();
+
+  # 現在のノード
+  currentNode1 = null;
+  currentNode2 = null;
+
+  # 選んだ動画のすべてのbufferSource
+  soundList = [];
+
+  # 音声ファイルのロード
+  # click時に再生
+  $('.start1').on 'click', ->
+    play(soundList[0].source, soundList, context)
+
+  $('.start2').on 'click', ->
+    source2.start(0)
+
+  $('.stop1').on 'click', ->
+    soundList[0].source.stop();
+
+  $('.stop2').on 'click', ->
+    source2.stop()
+
+  $('.load').on 'click', ->
+    loadSounds(SOUND_URLS, soundList, context).done ->
+      console.log(soundList)
+
